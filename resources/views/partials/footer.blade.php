8e863cd6cddef80145d32595a7927c1155a28674
alegreco7 <alexgreco2002@gmail.com>
2024-02-12 18:06:30 +0100

    footer lists, copyright section, sign-up button

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,18 +2,92 @@
     <div class="container">
         <div class="row justify-content-between">
             <div class="col-4">
+                <div class="lists-group">
+                    <div class="footer-list">
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Collectibles</li>
+                            <li>Videos</li>
+                            <li>Fans</li>
+                            <li>News</li>
+                            <li>Shop</li>
+                        </ul>
+                        <h4>SHOP</h4>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div class="footer-list">
+                        <h4>DC</h4>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Collectibles</li>
+                            <li>Videos</li>
+                            <li>Fans</li>
+                            <li>News</li>
+                            <li>Shop</li>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div class="footer-list">
+                        <h4>SITES</h4>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                        </ul>
+                    </div>
+                </div>
+                <p class="text-secondary">
+                    All site Content TM and &copy; 2020 DC Entertainments, unless otherwiste <span
+                        class="text-primary">noted here</span>. All rights reversed. <br>
+                    <span class="text-primary">Cookies Settings</span>
+                </p>
             </div>
             <div class="col-6">
+                <div class="bg-footer"></div>
             </div>
         </div>
     </div>
     <div class="bg-dark">
-        <div class="container">
-            <div class="row">
+        <div class="container py-5">
+            <div class="row justify-content-between">
                 <div class="col-3">
+                    <span class="my-btn">
+                        SIGN-UP NOW!
+                    </span>
                 </div>
-                <div class="col-6">
-                    <div class="bg-footer"></div>
+                <div class="col-4">
+                    <div class="d-flex gap-3 align-items-center">
+                        <h5 class="text-primary"> FOLLOW US</h5>
+                        <div class="icon">
+                            <img src="{{ Vite::asset('resources/images/footer-facebook.png') }}" alt="facebook">
+                        </div>
+                        <div class="icon">
+                            <img src="{{ Vite::asset('resources/images/footer-twitter.png') }}" alt="twitter">
+                        </div>
+                        <div class="icon">
+                            <img src="{{ Vite::asset('resources/images/footer-youtube.png') }}" alt="youtube">
+                        </div>
+                        <div class="icon">
+                            <img src="{{ Vite::asset('resources/images/footer-pinterest.png') }}" alt="pinterest">
+                        </div>
+                        <div class="icon">
+                            <img src="{{ Vite::asset('resources/images/footer-periscope.png') }}" alt="periscope">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
