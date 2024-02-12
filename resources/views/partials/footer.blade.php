8f7f94c6faf6743480f4350bcd0139d1465fb33b
alegreco7 <alexgreco2002@gmail.com>
2024-02-12 17:45:28 +0100

    start footer with bg

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-4">
+            </div>
+            <div class="col-6">
+            </div>
+        </div>
+    </div>
+    <div class="bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                </div>
+                <div class="col-6">
+                    <div class="bg-footer"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
