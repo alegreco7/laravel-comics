90daf45639d9e726f1c76f41a85286e368eb6ba2
alegreco7 <alexgreco2002@gmail.com>
2024-02-12 17:16:17 +0100

    start header and logo

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="bg-primary">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-6 col-md-4 col-lg-3">
+                    <span class="text-white">DC POWER&#8480; VISA&#174;</span>
+                </div>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <span class="text-white">ADDITIONAL DC SITES &#9662;</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container py-2">
+        <div class="row">
+            <div class="col-2">
+                <div class="logo">
+                    <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo">
+                </div>
+            </div>
+            <div class="col-10">
+            </div>
+        </div>
+    </div>
+</header>
